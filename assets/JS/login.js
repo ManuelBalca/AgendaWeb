170e4ee17214f7db5bfc13293be9daf065fe6b94
mbalcarcel2021016 <mbalcarcel-2021016@kinal.edu.gt>
2023-01-31 18:27:35 -0600

    Creacion del Log In

---
+++ assets/JS/login.js
@@ -0,0 +1,7 @@
+("a[data-toggle]").on("click", function(e) {
+    var selector = $(this).data("toggle");
+    if ((selector).css('display') != 'block') {
+      ('form').hide();
+    }
+    (selector).animate({height: "toggle", opacity: "toggle"}, "slow");
+  });
